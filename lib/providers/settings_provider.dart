44e3b67cf9a04d387f0011397b431b95822e73bb
faresaltaifiit-code <fares.altaifi.it@gmail.com>
2026-09-10 16:31:13 +0300

    إضافة lib/providers/settings_provider.dart

---
+++ lib/providers/settings_provider.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SettingsProvider extends ChangeNotifier {
+  static const _themeKey = 'theme_mode';
+  ThemeMode _themeMode = ThemeMode.system;
+  ThemeMode get themeMode => _themeMode;
+
+  Future<void> load() async {
+    final preferences = await SharedPreferences.getInstance();
+    _themeMode = switch (preferences.getString(_themeKey)) {
+      'light' => ThemeMode.light,
+      'dark' => ThemeMode.dark,
+      _ => ThemeMode.system,
+    };
+    notifyListeners();
+  }
+
+  Future<void> setThemeMode(ThemeMode value) async {
+    _themeMode = value;
+    notifyListeners();
+    final preferences = await SharedPreferences.getInstance();
+    await preferences.setString(_themeKey, value.name);
+  }
+}
+
