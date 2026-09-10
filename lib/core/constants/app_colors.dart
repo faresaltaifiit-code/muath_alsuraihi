cf05ee2fbbf86aea9a36bfcde0c7987cf3f7ffe1
faresaltaifiit-code <fares.altaifi.it@gmail.com>
2026-09-10 16:29:19 +0300

    إضافة lib/core/constants/app_colors.dart

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+/// لوحة الألوان الأساسية للتطبيق.
+abstract final class AppColors {
+  static const forestGreen = Color(0xFF143D32);
+  static const emerald = Color(0xFF28755F);
+  static const gold = Color(0xFFC99A3E);
+  static const softGold = Color(0xFFF3E6CA);
+
+  static const lightBackground = Color(0xFFFAF8F2);
+  static const lightSurface = Colors.white;
+  static const lightText = Color(0xFF18231F);
+  static const lightMutedText = Color(0xFF65706B);
+
+  static const darkBackground = Color(0xFF101916);
+  static const darkSurface = Color(0xFF1A2924);
+  static const darkText = Color(0xFFF5F3EC);
+  static const darkMutedText = Color(0xFFBBC6C0);
+}
+
