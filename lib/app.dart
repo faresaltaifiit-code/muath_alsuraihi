b9910604c9baefc772b4ef592ae76ad78a681284
faresaltaifiit-code <fares.altaifi.it@gmail.com>
2026-09-10 16:29:05 +0300

    إضافة lib/app.dart

---
+++ lib/app.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'core/constants/app_strings.dart';
+import 'core/theme/app_theme.dart';
+import 'features/home/presentation/home_screen.dart';
+import 'features/navigation/presentation/root_navigation_screen.dart';
+import 'providers/recitations_provider.dart';
+import 'providers/player_provider.dart';
+import 'providers/favorites_provider.dart';
+import 'providers/settings_provider.dart';
+
+class MuathAlsuraihiApp extends StatelessWidget {
+  const MuathAlsuraihiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => RecitationsProvider()..loadSurahs()),
+        ChangeNotifierProvider(create: (_) => PlayerProvider()..initialize()),
+        ChangeNotifierProvider(create: (_) => FavoritesProvider()..load()),
+        ChangeNotifierProvider(create: (_) => SettingsProvider()..load()),
+      ],
+      child: Consumer<SettingsProvider>(
+        builder: (context, settings, _) => MaterialApp(
+          title: AppStrings.appName,
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.light,
+          darkTheme: AppTheme.dark,
+          themeMode: settings.themeMode,
+          builder: (context, child) => Directionality(
+            textDirection: TextDirection.rtl,
+            child: child ?? const SizedBox.shrink(),
+          ),
+          home: const RootNavigationScreen(),
+        ),
+      ),
+    );
+  }
+}
+
