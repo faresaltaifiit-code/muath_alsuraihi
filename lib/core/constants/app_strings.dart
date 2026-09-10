c9922409d8bae8d406cbaf9583182a04305a05ea
faresaltaifiit-code <fares.altaifi.it@gmail.com>
2026-09-10 16:29:27 +0300

    إضافة lib/core/constants/app_strings.dart

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,6 @@
+abstract final class AppStrings {
+  static const appName = 'معاذ السريحي';
+  static const appDescription =
+      'تلاوات القارئ معاذ بن ماجد السريحي - المصحف المرتل (رمضان 1446هـ)';
+}
+
