4bc40b5aa4e3b64a571e0b46ec0e7084d996b8bb
faresaltaifiit-code <fares.altaifi.it@gmail.com>
2026-09-10 16:30:31 +0300

    إضافة lib/features/navigation/presentation/root_navigation_screen.dart

---
+++ lib/features/navigation/presentation/root_navigation_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../../home/presentation/home_screen.dart';
+import '../../library/presentation/library_screens.dart';
+import '../../more/presentation/more_screen.dart';
+import '../../surahs/presentation/surahs_screen.dart';
+
+class RootNavigationScreen extends StatefulWidget {
+  const RootNavigationScreen({super.key});
+  @override
+  State<RootNavigationScreen> createState() => _RootNavigationScreenState();
+}
+
+class _RootNavigationScreenState extends State<RootNavigationScreen> {
+  int _index = 0;
+  static const _screens = [HomeScreen(), SurahsScreen(), FavoritesScreen(), MoreScreen()];
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: IndexedStack(index: _index, children: _screens),
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: _index,
+          onDestinationSelected: (value) => setState(() => _index = value),
+          destinations: const [
+            NavigationDestination(icon: Icon(Icons.home_outlined), selectedIcon: Icon(Icons.home_rounded), label: 'الرئيسية'),
+            NavigationDestination(icon: Icon(Icons.menu_book_outlined), selectedIcon: Icon(Icons.menu_book_rounded), label: 'السور'),
+            NavigationDestination(icon: Icon(Icons.favorite_border_rounded), selectedIcon: Icon(Icons.favorite_rounded), label: 'المفضلة'),
+            NavigationDestination(icon: Icon(Icons.more_horiz_rounded), selectedIcon: Icon(Icons.more_rounded), label: 'المزيد'),
+          ],
+        ),
+      );
+}
+
