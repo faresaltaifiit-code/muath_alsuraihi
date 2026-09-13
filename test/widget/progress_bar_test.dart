c09618f6a351210024cef23d501d32fd38c1bbed
faresaltaifiit-code <fares.altaifi.it@gmail.com>
2026-09-13 09:41:11 +0300

    Add player widget tests

---
+++ test/widget/progress_bar_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('progress bar reflects a changed playback position', (tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: Slider(value: 30, max: 100, onChanged: null),
+    ));
+    expect(find.byType(Slider), findsOneWidget);
+    expect(tester.widget<Slider>(find.byType(Slider)).value, 30);
+  });
+}
